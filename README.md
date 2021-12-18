dc9d7e07e596b2eb0850130058a8a5d10d328ef7
Moritz R <c0derMo@users.noreply.github.com>
2021-12-18 23:56:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# BatchYoutubeDownloader
+A tool for downloading a list of YouTube videos, and cutting them to size with a set starting point & duration for each video.
+
+This tool uses [yt-dlp](https://github.com/yt-dlp/yt-dlp) and [ffmpy](https://github.com/Ch00k/ffmpy).
+ffmpy relies on FFmpeg, so make sure you have that installed and accessible in your path variable.
+
+# Usage
+1. Clone / download the repository
+2. Install the requirements using `pip install -r requirements.txt`
+3. Access the commandline interface using `python batchDownloader.py [LIST]`
+
+Supply the path to a list file in `[LIST]`.
+The list file should contain all your videos, with starting times & durations, formated as such:
+
+`[VIDEO URL] | [STARTING TIME] | [DURATION]`
+
+with times formatted as: `HH:MM:SS`.
+
+The tool will then do the following:
+1. Use yt-dlp to download the video to `./raw_downloads/`
+2. Use ffmpeg to remux & cut down the video, save that to `./output/`
+
+The files in `./output/` will be enumerated using the line numbers of the list file.
+
+## Commandline arguments:
+- `-h`: Displays information about the commandline arguments.
+- `-b` / `--begin_offset`: Offset the starting time of each video by subtracting the supplied amount of seconds off the starting time.
+- `-d` / `--duration_offset`: Offset the duration of each video by adding the supplied amount of seconds to the duration.
+- `-o` / `--override`: Override already existing files in `./output/`.
+- `-i` / `--ignore_errors`: Ignore the errors in the supplied list file. The lines with errors will be ignored.
+- `-c` / `--clean`: Clean the `./raw_downloads/`-folder upon finishing, to free up a bunch of disk space since all the full-length video files are stored there before getting cut to size.
---
+++ batchDownloader.py
@@ -0,0 +1,123 @@
+from ffmpy import FFmpeg, FFRuntimeError
+from yt_dlp import YoutubeDL
+from pathlib import Path
+import argparse
+import sys
+import re
+import os
+import glob
+
+def download_hook(d, starting_time, duration, final_filename, override):
+    if d.get("status") == "finished":
+        print("")
+        print("Finished downloading video as " + d.get("filename") + ".")
+        print("Cutting video " + d.get("filename") + " from starting time " + starting_time + " to duration " + duration + "...")
+        cut_video(d.get("filename"), starting_time, duration, final_filename, override)
+        print("Saved final video as " + final_filename + ".")
+
+
+def download_video(video_link, starting_time, duration, final_filename, override):
+    ydl_opts = {
+        'progress_hooks': [lambda x: download_hook(x, starting_time, duration, final_filename, override)],
+        'format': 'best',
+        'outtmpl': './raw_downloads/%(id)s.mp4',
+        'quiet': 'true'
+    }
+    print("Downloading " + video_link + "...")
+    result = YoutubeDL(ydl_opts).download([video_link])
+
+
+def cut_video(video_name, starting_time, duration, filename, override):
+    if override:
+        global_options = "-y -hide_banner -loglevel error"
+    else:
+        global_options = "-n -hide_banner -loglevel error"
+    try:
+        FFmpeg(
+            global_options=global_options,
+            inputs={video_name: None},
+            outputs={'./output/' + filename: '-ss ' + starting_time + ' -t ' + duration + ' -c copy'}
+        ).run()
+    except FFRuntimeError:
+        print("Error when cutting video " + video_name + "!")
+
+
+def add_to_time(time, hours=0, minutes=0, seconds=0):
+    """
+    Add time to a time-string of the format HH:MM:SS
+    """
+    split_time = time.split(":")
+    in_seconds = int(split_time[0]) * 3600 + int(split_time[1]) * 60 + int(split_time[2])
+    in_seconds += hours * 3600 + minutes * 60 + seconds
+
+    new_hours = in_seconds // 3600
+    in_seconds %= 3600
+    new_minutes = in_seconds // 60
+    in_seconds %= 60
+    return "%02i:%02i:%02i" % (new_hours, new_minutes, in_seconds)
+
+
+def subtract_from_time(time, hours=0, minutes=0, seconds=0):
+    """
+    Subtract time from a time-string of the format HH:MM:SS
+    """
+    split_time = time.split(":")
+    in_seconds = int(split_time[0]) * 3600 + int(split_time[1]) * 60 + int(split_time[2])
+    in_seconds -= hours * 3600 + minutes * 60 + seconds
+    if in_seconds < 0:
+        return "00:00:00"
+
+    new_hours = in_seconds // 3600
+    in_seconds %= 3600
+    new_minutes = in_seconds // 60
+    in_seconds %= 60
+    return "%02i:%02i:%02i" % (new_hours, new_minutes, in_seconds)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Download a list of YouTube videos and cut them to a set size.')
+    parser.add_argument('list', type=str, help="Path to a list file containing video-links, starting timestamps and durations in the format '[LINK] | [STARTING TIME] | [DURATION]' with times supplied in HH:MM:SS.")
+    parser.add_argument('-b', '--begin_offset', type=int, help="Offsets the starting time of the videos by subtracting the supplied amount of seconds.", default=0)
+    parser.add_argument('-d', '--duration_offset', type=int, help="Offsets the duration of the videos by adding the supplied amount of seconds.", default=0)
+    parser.add_argument('-o', '--override', action='store_true', help="Override existing files in output folder.")
+    parser.add_argument('-i', '--ignore_errors', action='store_true', help="Ignore errors in the supplied list. Invalid lines will be fully ignored.")
+    parser.add_argument('-c', '--clean', action='store_true', help="Clean the raw_downloads directory after finishing, removing all the non-cut videos to free up disk space.")
+    args = parser.parse_args()
+
+    Path("./raw_downloads").mkdir(exist_ok=True)
+    Path("./output").mkdir(exist_ok=True)
+
+    try:
+        with open(args.list, 'r') as f:
+            video_list = f.read().split("\n")
+            f.close()
+    except FileNotFoundError:
+        print("List " + args.list + " not found! Exiting.")
+        sys.exit(1)
+    
+    parsed_video_list = []
+    for (line_number, video_line) in enumerate(video_list, 1):
+        splitted_line = video_line.split(" | ")
+        if len(splitted_line) != 3:
+            print("Line " + str(line_number) + " isn't formatted correctly! Expected colums: 3. Found: " + str(len(splitted_line)))
+            if not args.ignore_errors:
+                sys.exit(1)
+            else:
+                continue
+        if not re.match("\d+:\d+:\d+", splitted_line[1]) or not re.match("\d+:\d+:\d+", splitted_line[2]):
+            print("Line " + str(line_number) + " isn't formatted correctly! Times aren't formatted as HH:MM:SS.")
+            if not args.ignore_errors:
+                sys.exit(1)
+            else:
+                continue
+        parsed_video_list.append((splitted_line[0], subtract_from_time(splitted_line[1], seconds=args.begin_offset), add_to_time(splitted_line[2], seconds=args.duration_offset)))
+    
+    print("Parsed " + args.list + " with " + str(len(parsed_video_list)) + " videos.")
+    for (idx, video) in enumerate(parsed_video_list, 1):
+        print("==[ Video #" + str(idx) + " ]==")
+        download_video(video[0], video[1], video[2], str(idx) + ".mp4", args.override)
+    print("Finished downloading videos!")
+    if args.clean:
+        print("Cleaning ./raw_downloads folder...")
+        for files in glob.glob("./raw_downloads/*"):
+            os.remove(files)
+        print("Done!")
